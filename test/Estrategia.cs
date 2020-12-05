5db2629bb8060d05c40e2a4b47f1f5c5778f3bdf
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-12-05 13:19:05 -0300

    Implemento la consulta3

--- test/Estrategia.cs
+++ test/Estrategia.cs
@@ -41,13 +41,38 @@ namespace DeepSpace
 				}
 				mensaje += "Nivel " + nivel + ": " + cantidadPorNivel + "\n";
 			}
-			return mensaje; ;
+			return mensaje; 
 		}
 
 
 		public String Consulta3( ArbolGeneral<Planeta> arbol)
 		{
-			return "Implementar";
+			Cola<ArbolGeneral<Planeta>> q = new Cola<ArbolGeneral<Planeta>>();
+			q.encolar(arbol);
+			int nivel = 0;
+			
+			String mensaje = "";
+			while (!q.esVacia())
+			{
+				int elementos = q.cantElementos();
+				nivel++;
+				int cantidadPorNivel = 0;
+				int poblacionPorNivel = 0;
+				while (elementos-- > 0)
+				{
+					ArbolGeneral<Planeta> nodoActual = q.desencolar();
+
+					cantidadPorNivel++;
+					poblacionPorNivel += nodoActual.getDatoRaiz().Poblacion();
+
+					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos())
+					{
+						q.encolar(hijo);
+					}
+				}
+				mensaje += "Nivel " + nivel + ": " + poblacionPorNivel/cantidadPorNivel + "\n";
+			}
+			return mensaje; 
 		}
 		
 		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
