3df9f1ce14f77c24dd908da8d7e6e4ef3842069f
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-12-08 16:51:30 -0300

    Planteo una parte del metodo CalcularMovimiento.
    El bot ahora decide ir hasta la raíz. Ademas, utilizo el método caminoRaizIa que construye un camino desde la raíz hasta el primer planeta perteneciente al bot.

--- test/Estrategia.cs
+++ test/Estrategia.cs
@@ -6,65 +6,43 @@ namespace DeepSpace
 
 	class Estrategia
 	{
-		
-		
-		public String Consulta1( ArbolGeneral<Planeta> arbol)
-		{
+		public String Consulta1( ArbolGeneral<Planeta> arbol){//Recorrido por niveles
 			Cola<ArbolGeneral<Planeta>> q = new Cola<ArbolGeneral<Planeta>>();
 			q.encolar(arbol);
 			int nivel = 0;
-			
-			
-			while (!q.esVacia())
-			{
+			while (!q.esVacia()){
 				int elementos = q.cantElementos();
 				nivel++;
-				
-				
-				while (elementos-- > 0)
-				{
+				while (elementos-- > 0){
 					ArbolGeneral<Planeta> nodoActual = q.desencolar();
-
-					if (nodoActual.getDatoRaiz().EsPlanetaDeLaIA()) {
+					if (nodoActual.getDatoRaiz().EsPlanetaDeLaIA()){
 						nivel--;
 						string texto = nivel.ToString();
 						return "La distancia entre la raiz y el planeta del bot es de: " + texto;
 					}
-                   
-						foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos())
-						{
-							q.encolar(hijo);
-						}
-
-					
+					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos()){
+						q.encolar(hijo);
+					}
 				}
 			}
 			return "";
 		}
-
-
-		public String Consulta2( ArbolGeneral<Planeta> arbol)
-		{
+			
+		public String Consulta2( ArbolGeneral<Planeta> arbol){//Recorrido por niveles
 			Cola<ArbolGeneral<Planeta>> q = new Cola<ArbolGeneral<Planeta>>();
 			q.encolar(arbol);
 			int nivel = 0;
 			String mensaje = "";
-			while (!q.esVacia())
-			{
+			while (!q.esVacia()){
 				int elementos = q.cantElementos();
 				nivel++;
 				int cantidadPorNivel = 0;
-				while (elementos-- > 0)
-				{
+				while (elementos-- > 0){
 					ArbolGeneral<Planeta> nodoActual = q.desencolar();
-
-					if (nodoActual.getDatoRaiz().Poblacion() > 10)
-					{
+					if (nodoActual.getDatoRaiz().Poblacion() > 10){
 						cantidadPorNivel++;
 					}
-
-					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos())
-					{
+					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos()){
 						q.encolar(hijo);
 					}
 				}
@@ -72,30 +50,22 @@ namespace DeepSpace
 			}
 			return mensaje; 
 		}
-
-
-		public String Consulta3( ArbolGeneral<Planeta> arbol)
-		{
+		
+		public String Consulta3( ArbolGeneral<Planeta> arbol){//Recorrido por niveles
 			Cola<ArbolGeneral<Planeta>> q = new Cola<ArbolGeneral<Planeta>>();
 			q.encolar(arbol);
 			int nivel = 0;
-			
 			String mensaje = "";
-			while (!q.esVacia())
-			{
+			while (!q.esVacia()){
 				int elementos = q.cantElementos();
 				nivel++;
 				int cantidadPorNivel = 0;
 				int poblacionPorNivel = 0;
-				while (elementos-- > 0)
-				{
+				while (elementos-- > 0){
 					ArbolGeneral<Planeta> nodoActual = q.desencolar();
-
 					cantidadPorNivel++;
 					poblacionPorNivel += nodoActual.getDatoRaiz().Poblacion();
-
-					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos())
-					{
+					foreach (ArbolGeneral<Planeta> hijo in nodoActual.getHijos()){
 						q.encolar(hijo);
 					}
 				}
@@ -103,11 +73,32 @@ namespace DeepSpace
 			}
 			return mensaje; 
 		}
-		
-		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
-		{
-			//Implementar
-			
+
+		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol){
+            if (!arbol.getDatoRaiz().EsPlanetaDeLaIA()){//Si la raiz no pertenece al bot
+				List<Planeta> caminoIA = new List<Planeta>();//Creo el camino
+				caminoIA = this.caminoRaizIa(arbol,caminoIA);
+				int count = caminoIA.Count;
+				Movimiento ataque = new Movimiento(caminoIA[count - 1], caminoIA[count - 2]);//Realizo el ataque 
+				return ataque;
+			}
+			return null;
+		}
+
+		//El metodo caminoRaizIa consigue un camino desde la raiz al primer planeta del bot
+		private List<Planeta> caminoRaizIa(ArbolGeneral<Planeta> arbol, List<Planeta> camino){
+			camino.Add(arbol.getDatoRaiz());//Agrego el planeta al camino
+			if (arbol.getDatoRaiz().EsPlanetaDeLaIA()){//Si encuentro el planeta de la IA retorno el camino
+				return camino;
+            }else{
+				foreach (ArbolGeneral<Planeta> hijo in arbol.getHijos()){
+					List<Planeta> caminoAux = this.caminoRaizIa(hijo, camino);
+                    if (caminoAux!=null){//Si esta condicion es verdadera significa que el metodo encontro el planeta del bot
+						return caminoAux; 
+                    }
+					camino.RemoveAt(camino.Count - 1);//En caso de llegar a un camino equivocado elimino el ultimo elemento 
+				}
+			}
 			return null;
 		}
 	}
